c105f02962eef18e4d14cb3bdcdf3286fb1412a4
Ray <Rmuhamar@gmail.com>
2020-11-06 13:35:26 +0700

    Initial Commit

---
+++ Tes SPE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tes SPE
+//
+//  Created by Nusantara Card Semesta on 06/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
